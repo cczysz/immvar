930faeda8c428a1acf16061485b2badf56f2bad5
Charles Czysz <charles.czysz@gmail.com>
2015-08-17 10:19:05 -0500

    converted to functions

--- DE_analysis/de_analysis.R
+++ DE_analysis/de_analysis.R
@@ -1,75 +1,23 @@
-# Initially, testing probe removal for Caucasian individuals in the CD14 celltype
-## Will be expanded after testing
-
-# This file will source other scripts which each perform probe removal, normalization, PEER analysis, and differential expression
-
-# Sys.setenv(R_THREADS=4)
 library(oligo)
 library(limma)
 library(ggplot2)
 
-# Populations: Caucasian, African-American, Asian
-# Cell types: CD14, CD4
-LoadData <- function(population,cell.type) {
-if (F) {
-#files.dir = "/group/stranger-lab/nicolel/mRNA_expression/CEL_files/CD14/"
-#setwd(files.dir)
-
-out.dir = '/scratch/t.cczysz/'
-
-phenotype.file = "CD14.Samples.POSTQC.ImmVarFinal.txt"
-probe.info = "/home/t.cri.cczysz/HuGeneProbeInfo.csv"
-
-# Save files
-peer.factors.f = "/home/t.cri.cczysz/peer_factors.Robj"
-residual.exp.f = "/home/t.cri.cczysz/residuals.Robj"
-# raw_exp_rfile = "cd4_cau_raw.RData"
-# norm_exp_rfile = "cd4_cau_expr.RData"
-
-samples <- read.csv(phenotype.file, header=T)
-
-data.cau <- samples[samples$Race == 'Caucasian', ]
-#data.subset <- data.cau[sample(1:nrow(data.cau), 10), ]
-data.subset <- data.cau
-
-data.ids.subset <- data.subset[,1]
-data.files.subset <- data.subset[,2]
-data.sex.subset <- data.subset$Sex
-}
-
+population <<- "Caucasian"
+cell.type <<- "CD14"
 load('/group/stranger-lab/moliva/ImmVar/Robjects/phen.Robj')
 phen <- phen[phen$CellType == 'CD14+16-Mono', ]
 
-# PEER files
-# Required for PEER:
-	# Raw Expression as `expression`
-	# Vector of sex covariates as `sex`
-# Outputs:
-	# Factors: #(Genes)x#(Factors) matrix of PEER factors for use in linear model
-
-#load('/scratch/t.cczysz/exp_genes.Robj')
-load('/group/stranger-lab/moliva/ImmVar/Robjects/exp_genes.Robj')
-expression <- exp_genes
-
-if (F) {
-if (file.exists(file=peer.factors.f)) load(file=peer.factors.f) else {
-  source('/home/t.cri.cczysz/thesis/scripts/peer.R')
-  sex <- as.numeric(phen[phen$Race == 'Caucasian', ]$Sex == 'Male')
-  peer.factors <- RunPeer(expression,k=20,sex)
-  # save(peer.factors, file=peer.factors.f) 
-}
-save(peer.factors, file='/scratch/t.cczysz/peer_factors.Robj') 
+# Populations: Caucasian, African-American, Asian
+# Cell types: CD14, CD4
+# Imported as "exp_genes": GxN matrix of normalized expression values
+LoadData <- function(population=population,cell.type=cell.type) {
+	data.dir <- "/group/stranger-lab/moliva/ImmVar/Robjects/"
+	file.path <- paste(data.dir,"exp.genes.",cell.type,".",population,".Robj",sep="")
+	load(file=file.path)
 }
 
-### Calculating Residuals
-if (F) {
-if (file.exists(file=residual.exp.f)) load(file=residual.exp.f) else {
-	MakeResiduals <- function(input.row,peer.factors) {
-		residuals(lm(input.row ~ 0 + peer.factors[, -1]))
-	}
-	expr.residuals <- apply(expression, 1, MakeResiduals, peer.factors=peer.factors)
-	expr.residuals <- t(expr.residuals)
-}
+MakeResiduals <- function(input.row,peer.factors) {
+	residuals(lm(input.row ~ 0 + peer.factors[, -1]))
 }
 
 PerformDEAnalysis <- function(expr,samples) {
@@ -83,20 +31,33 @@ PerformDEAnalysis <- function(expr,samples) {
 	eb.fit <- eBayes(contrast.fit, robust=F)
 }
 
+AnalyzeFit <- function(eb.fit) {
+	top.de.genes <- row.names(topTable(eb.fit, number=100))
+
+	DE_probesets <- unique(merge_probes_DF[merge_probes_DF$gene_ensembl%in%top.de.genes, ]$fsetid)
+
+	g = ggplot(data=data.frame(eb.fit),aes(x=coefficients,y=lods)) 
+	pdf('/home/t.cri.cczysz/volcano.pdf')
+	g + geom_point() + xlab("fold change") + ylab("log odds")
+	dev.off() 
+
+	pdf(file='/home/t.cri.cczysz/de_probesets.pdf')
+	for (set in top.de.genes) {
+		plot(density(expr.residuals[set, !!sex]),col='blue',xlim=c(-1,1),ylim=c(0,1))
+		lines(density(expr.residuals[set, !sex]),col='red')
+	}
+	dev.off()
+}
+
+LoadData()
+
+source('/home/t.cri.cczysz/thesis/scripts/peer.R')
+sex <- as.numeric(phen[phen$Race == 'Caucasian', ]$Sex == 'Male')
+peer.factors <- RunPeer(exp_genes,k=20,sex)
+
+expr.residuals <- apply(expression, 1, MakeResiduals, peer.factors=peer.factors)
+expr.residuals <- t(expr.residuals)
+
 eb.fit <- PerformDEAnalysis(expr.residuals,as.factor(sex))
 
-top.de.genes <- row.names(topTable(eb.fit, number=100))
-
-DE_probesets <- unique(merge_probes_DF[merge_probes_DF$gene_ensembl%in%top.de.genes, ]$fsetid)
-
-g = ggplot(data=data.frame(eb.fit),aes(x=coefficients,y=lods)) 
-pdf('/home/t.cri.cczysz/volcano.pdf')
-g + geom_point() + xlab("fold change") + ylab("log odds")
-dev.off() 
-
-pdf(file='/home/t.cri.cczysz/de_probesets.pdf')
-for (set in top.de.genes) {
-	plot(density(expr.residuals[set, !!sex]),col='blue',xlim=c(-1,1),ylim=c(0,1))
-	lines(density(expr.residuals[set, !sex]),col='red')
-}
-dev.off()
+AnalyzeFit(eb.fit)
