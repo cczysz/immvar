0b9fd5529b37a192bf561b9c8a889ec83690b984
Charles Czysz <charles.czysz@gmail.com>
2015-08-24 11:53:04 -0500

    edit scripts for ftest

---
+++ sub_scripts/generateFilteredGeneExp.bash
@@ -0,0 +1,12 @@
+echo "Caucasian CD14"
+./generateFilteredGeneExp.R -p Caucasian -c CD14;
+echo "Caucasian CD4"
+./generateFilteredGeneExp.R -p Caucasian -c CD4;
+echo "Asian CD14"
+./generateFilteredGeneExp.R -p Asian -c CD14;
+echo "Asian CD4"
+./generateFilteredGeneExp.R -p Asian -c CD4;
+echo "African-American CD14"
+./generateFilteredGeneExp.R -p African-American -c CD14;
+echo "African-American CD4"
+./generateFilteredGeneExp.R -p African-American -c CD4;
