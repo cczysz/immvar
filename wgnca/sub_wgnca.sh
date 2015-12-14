9004fb59a72147d33a918b706c845acc616bb74b
Charles Czysz <charles.czysz@gmail.com>
2015-12-14 14:41:26 -0600

    updated

--- wgnca/sub_wgnca.sh
+++ wgnca/sub_wgnca.sh
@@ -3,7 +3,7 @@
 #PBS -N czysz_wgnca
 #PBS -S /bin/bash
 #PBS -l walltime=10:00:00
-#PBS -l nodes=1:ppn=1
+#PBS -l nodes=1:ppn=4
 #PBS -l mem=20gb
 
 #PBS -o $HOME/
--- wgnca/wgnca.R
+++ wgnca/wgnca.R
@@ -1,178 +1,113 @@
 library(WGCNA)
+options(stringsAsFactors = FALSE);
+enableWGCNAThreads(nThreads=4)
 
-#datExpr <- expression data
-#######
-# Graphically determine which soft threshold to use
-# Choose the lowest threshold closest to a fit index of 0.9
+loadExpression <- function(celltype) {
+	setwd('/group/stranger-lab/moliva/ImmVar/Robjects')
+	#for (cell in c('CD14', 'CD4')) {
 
-# Choose a set of soft-thresholding powers
+	load('phen.Robj')
+	if (celltype == 'CD14') {phen <-subset(phen, CellType=="CD14+16-Mono")} else {phen <- subset(phen, CellType=="CD4TNve") }
 
-setwd('/group/stranger-lab/moliva/ImmVar/Robjects')
-#for (cell in c('CD14', 'CD4')) {
-for (cell in c('CD14')) {
+	load(file=paste(celltype, '.joint.norm.exp_genes.Robj', sep=''))
+	if (celltype == 'CD14') { expr <- exp_genes.cd14.joint.norm } else { expr <- exp_genes.cd4.joint.norm }
 
-load(file=paste(cell, '.joint.norm.exp_genes.Robj', sep=''))
-if (cell == 'CD14') { expr <- exp_genes.cd14.joint.norm } else { expr <- exp_genes.cd4.joint.norm }
+	expr <- list(male=t(expr[, phen$Sex=='Male']), female=t(expr[, phen$Sex=='Female']))
+	return(expr)
+}
 
-expr <- t(expr)
+createTOM <- function(expr) {
+	powers = c(c(1:10), seq(from = 12, to=20, by=2))
+	sft = pickSoftThreshold(expr, powerVector = powers, verbose = 5)
+	softPower = which(-sign(sft$fitIndices[,3])*sft$fitIndices[,2]>0.8)[1];
+	adjacency = adjacency(expr, power = softPower);
+	dissTOM = 1-TOMsimilarity(adjacency)
+	return(dissTOM)
+}
+
+identifyModules <- function(geneTree, dissTOM, minModuleSize=30) {
+
+	dynamicMods = cutreeDynamic(dendro = geneTree, distM = dissTOM,
+		deepSplit = 2, pamRespectsDendro = FALSE,
+		minClusterSize = minModuleSize);
+}
+
+performGOAnalysis <- function(expr, moduleColors) {
+
+	geneNames.ens <- colnames(expr)
+	geneNames.sym <- as.character(annots[geneNames.ens, 'symbol_id'])
+	ensembl = useMart("ENSEMBL_MART_ENSEMBL",dataset="hsapiens_gene_ensembl", host="www.ensembl.org")
+	biomart.results=getBM(ensembl,attributes=c("ensembl_gene_id","hgnc_symbol","entrezgene"),filters="ensembl_gene_id",values=substring(geneNames.ens,1,15))
+	uni=biomart.results[!duplicated(biomart.results[,1]),]
+
+	matchings <- (substring(geneNames.ens,1,15)%in%uni[,1])
+	geneNames.ens <- geneNames.ens[matchings]
+	moduleColors.go <- moduleColors[matchings]
+
+
+	GOenr <- GOenrichmentAnalysis(moduleColors.go, uni[, 3])
+}
 
 setwd('/group/stranger-lab/immvar/wgnca')
 
-powers = c(c(1:10), seq(from = 12, to=20, by=2))
+for (celltype in c('CD14', 'CD4')) {
+	exp.list <- loadExpression(celltype)
+for (i in seq(length(exp.list))) {
 
-# Call the network topology analysis function
+	pdf.prefix <- paste(celltype, names(exp.list)[i], sep='_')
+	print(pdf.prefix)
+	dissTOM <- createTOM(exp.list[[i]])
+	geneTree = hclust(as.dist(dissTOM), method = "average");
 
-sft = pickSoftThreshold(expr, powerVector = powers, verbose = 5)
+	dynamicMods <- identifyModules(geneTree, dissTOM, 30)
+	dynamicColors = labels2colors(dynamicMods)
 
-# Plot the results:
+	pdf(file=paste(pdf.prefix, 'dendogram.pdf', sep='.'), width=8, height=6)
+	plotDendroAndColors(geneTree, dynamicColors, "Dynamic Tree Cut",
+	dendroLabels = FALSE, hang = 0.03,
+	addGuide = TRUE, guideHang = 0.05,
+	main = "Gene dendrogram and module colors")
+	dev.off()
+	
+	MEList = moduleEigengenes(expr, colors = dynamicColors)
+	MEs = MEList$eigengenes
+	# Calculate dissimilarity of module eigengenes
+	MEDiss = 1-cor(MEs);
+	# Cluster module eigengenes
+	METree = hclust(as.dist(MEDiss), method = "average");
 
-if (F) {
-#sizeGrWindow(9, 5)
-pdf(file='threshold.pdf', width=9, height=5)
-par(mfrow = c(1,2));
-cex1 = 0.9;
+	pdf(file=paste(pdf.prefix, 'eigengene_tree.pdf', sep='.'), width=7, height=6)
+	plot(METree, main = "Clustering of module eigengenes",
+	xlab = "", sub = "")
+	MEDissThres = 0.25
+	# Plot the cut line into the dendrogram
+	abline(h=MEDissThres, col = "red")
+	dev.off()
 
-# Scale-free topology fit index as a function of the soft-thresholding power
-plot(sft$fitIndices[,1], -sign(sft$fitIndices[,3])*sft$fitIndices[,2],
-xlab="Soft Threshold (power)",ylab="Scale Free Topology Model Fit,signed R^2",type="n",
-main = paste("Scale independence"));
-text(sft$fitIndices[,1], -sign(sft$fitIndices[,3])*sft$fitIndices[,2],
-labels=powers,cex=cex1,col="red");
-# this line corresponds to using an R^2 cut-off of h
-abline(h=0.90,col="red")
-# Mean connectivity as a function of the soft-thresholding power
-plot(sft$fitIndices[,1], sft$fitIndices[,5],
-xlab="Soft Threshold (power)",ylab="Mean Connectivity", type="n",
-main = paste("Mean connectivity"))
-text(sft$fitIndices[,1], sft$fitIndices[,5], labels=powers, cex=cex1,col="red")
-dev.off()
+	merge = mergeCloseModules(expr, dynamicColors, cutHeight = MEDissThres, verbose = 3)
+	# The merged module colors
+	mergedColors = merge$colors;
+	# Eigengenes of the new merged modules:
+	mergedMEs = merge$newMEs;
+	moduleColors = mergedColors
+	# Construct numerical labels corresponding to the colors
+	colorOrder = c("grey", standardColors(50));
+	moduleLabels = match(moduleColors, colorOrder)-1;
+	MEs = mergedMEs;
+
+	pdf(file=paste(pdf.prefix, 'dendogram.pdf', sep='.'), width=8, height=6)
+	plotDendroAndColors(geneTree, cbind(dynamicColors, mergedColors),
+		c("Dynamic Tree Cut", "Merged dynamic"),
+		dendroLabels = FALSE, hang = 0.03,
+		addGuide = TRUE, guideHang = 0.05)
+	dev.off()
+	
+	plotTOM <- dissTOM^7
+	diag(plotTOM) = NA
+	pdf(file=paste(pdf.prefix, 'TOM.cormat.pdf', sep='_'), width=9, height=9)
+	TOMplot(plotTOM, geneTree, moduleColors)
+	dev.off()
+
+	save.image(file=paste(pdf.prefix, 'Robj', sep='.'))
 }
-
-# Create adjacency matrix
-#softPower = 6;
-softPower = which(-sign(sft$fitIndices[,3])*sft$fitIndices[,2]>0.8)[1];
-
-adjacency = adjacency(expr, power = softPower);
-
-# Turn adjacency into topological overlap
-TOM = TOMsimilarity(adjacency);
-dissTOM = 1-TOM
-
-# Call the hierarchical clustering function
-geneTree = hclust(as.dist(dissTOM), method = "average");
-# Plot the resulting clustering tree (dendrogram)
-pdf(file='tom_clustering.pdf', width=12, height=8)
-plot(geneTree, xlab="", sub="", main = "Gene clustering on TOM-based dissimilarity", labels = FALSE, hang = 0.04);
-dev.off()
-
-# We like large modules, so we set the minimum module size relatively high:
-minModuleSize = 30;
-# Module identification using dynamic tree cut:
-dynamicMods = cutreeDynamic(dendro = geneTree, distM = dissTOM,
-	deepSplit = 2, pamRespectsDendro = FALSE,
-	minClusterSize = minModuleSize);
-#table(dynamicMods)
-
-# Convert numeric lables into colors
-dynamicColors = labels2colors(dynamicMods)
-#table(dynamicColors)
-# Plot the dendrogram and colors underneath
-#sizeGrWindow(8,6)
-
-pdf(file='dendogram.pdf', width=8, height=6)
-plotDendroAndColors(geneTree, dynamicColors, "Dynamic Tree Cut",
-dendroLabels = FALSE, hang = 0.03,
-addGuide = TRUE, guideHang = 0.05,
-main = "Gene dendrogram and module colors")
-dev.off()
-
-# Calculate eigengenes
-MEList = moduleEigengenes(expr, colors = dynamicColors)
-MEs = MEList$eigengenes
-# Calculate dissimilarity of module eigengenes
-MEDiss = 1-cor(MEs);
-# Cluster module eigengenes
-METree = hclust(as.dist(MEDiss), method = "average");
-# Plot the result
-pdf(file='dendro.pdf', width=7, height=6)
-plot(METree, main = "Clustering of module eigengenes",
-xlab = "", sub = "")
-MEDissThres = 0.25
-# Plot the cut line into the dendrogram
-abline(h=MEDissThres, col = "red")
-dev.off()
-# Call an automatic merging function
-merge = mergeCloseModules(datExpr, dynamicColors, cutHeight = MEDissThres, verbose = 3)
-# The merged module colors
-mergedColors = merge$colors;
-# Eigengenes of the new merged modules:
-mergedMEs = merge$newMEs;
-
-#sizeGrWindow(12, 9)
-pdf(file = "geneDendro-3.pdf", wi = 9, he = 6)
-plotDendroAndColors(geneTree, cbind(dynamicColors, mergedColors),
-c("Dynamic Tree Cut", "Merged dynamic"),
-dendroLabels = FALSE, hang = 0.03,
-addGuide = TRUE, guideHang = 0.05)
-dev.off()
-
-# Rename to moduleColors
-moduleColors = mergedColors
-# Construct numerical labels corresponding to the colors
-colorOrder = c("grey", standardColors(50));
-moduleLabels = match(moduleColors, colorOrder)-1;
-MEs = mergedMEs;
-# Save module colors and labels for use in subsequent parts
-save(MEs, moduleLabels, moduleColors, geneTree, file = paste(cell, 'dat.Robj', sep='.'))
-
-nGenes = ncol(expr);
-nSamples = nrow(expr);
-
-MEs0 = moduleEigengenes(expr, moduleColors)$eigengenes
-MEs = orderMEs(MEs0)
-moduleTraitCor = cor(MEs, datTraits, use = "p");
-moduleTraitPvalue = corPvalueStudent(moduleTraitCor, nSamples);
-
-pdf(file="mod_trait_rel.pdf", width=12, height=8)
-textMatrix = paste(signif(moduleTraitCor, 2), "\n(", signif(moduleTraitPvalue, 1), ")", sep = "");
-dim(textMatrix) = dim(moduleTraitCor)
-par(mar = c(6, 8.5, 3, 3));
-# Display the correlation values within a heatmap plot
-labeledHeatmap(Matrix = moduleTraitCor,
-xLabels = names(datTraits),
-yLabels = names(MEs),
-ySymbols = names(MEs),
-colorLabels = FALSE,
-colors = greenWhiteRed(50),
-textMatrix = textMatrix,
-setStdMargins = FALSE,
-cex.text = 0.5,
-zlim = c(-1,1),
-main = paste("Module-trait relationships"))
-dev.off()
-
-weight = as.data.frame(datTraits$weight_g);
-names(weight) = "weight"
-# names (colors) of the modules
-modNames = substring(names(MEs), 3)
-geneModuleMembership = as.data.frame(cor(datExpr, MEs, use = "p"));
-MMPvalue = as.data.frame(corPvalueStudent(as.matrix(geneModuleMembership), nSamples));
-names(geneModuleMembership) = paste("MM", modNames, sep="");
-names(MMPvalue) = paste("p.MM", modNames, sep="");
-geneTraitSignificance = as.data.frame(cor(datExpr, weight, use = "p"));
-GSPvalue = as.data.frame(corPvalueStudent(as.matrix(geneTraitSignificance), nSamples));
-names(geneTraitSignificance) = paste("GS.", names(weight), sep="");
-names(GSPvalue) = paste("p.GS.", names(weight), sep="");
-
-module = "brown"
-column = match(module, modNames);
-moduleGenes = moduleColors==module;
-sizeGrWindow(7, 7);
-par(mfrow = c(1,1));
-verboseScatterplot(abs(geneModuleMembership[moduleGenes, column]),
-abs(geneTraitSignificance[moduleGenes, 1]),
-xlab = paste("Module Membership in", module, "module"),
-ylab = "Gene significance for body weight",
-main = paste("Module membership vs. gene significance\n"),
-cex.main = 1.2, cex.lab = 1.2, cex.axis = 1.2, col = module)
 }
